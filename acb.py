2361370fa524b5ec1605ee2c45dc2120aa662d87
alptangal <alptangal@gmail.com>
2026-02-08 13:28:46 +0700

    .

--- acb.py
+++ acb.py
@@ -13,37 +13,41 @@ from bs4 import BeautifulSoup as Bs4
 
 
 async def login(user, password):
-    url = "https://apiapp.acb.com.vn/mb/v2/auth/tokens"
-    data = {
-        "username": user,
-        "password": password,
-        "clientId": "iuSuHYVufIUuNIREV0FB9EoLn9kHsDbm",
-    }
-    headers = {"user-agent": "ACB-MBA/5 CFNetwork/1325.0.1 Darwin/21.1.0"}
-    req = requests.post(url, headers=headers, json=data)
-    print(req)
-    async with aiohttp.ClientSession(cookie_jar=aiohttp.CookieJar()) as session:
-        async with session.post(url, headers=headers, json=data) as res:
-            if res.status < 400:
-                js = await res.json()
-                headers["authorization"] = "Bearer " + js["refreshToken"]
-                refreshTk = js["refreshToken"]
-                url = "https://apiapp.acb.com.vn/mb/v2/auth/refresh"
-                async with aiohttp.ClientSession(
-                    cookie_jar=aiohttp.CookieJar()
-                ) as session:
-                    async with session.post(url, headers=headers) as res:
-                        url = "https://apiapp.acb.com.vn/mb/legacy/ss/cs/bankservice/transfers/list/account-payment"
-                        if res.status < 400:
-                            js = await res.json()
-                            headers["authorization"] = "Bearer " + js["accessToken"]
-                            print(user + " login success")
-                            return {
-                                "headers": headers,
-                                "refreshTk": refreshTk,
-                                "username": user,
-                            }
-            return False
+    try:
+        url = "https://apiapp.acb.com.vn/mb/v2/auth/tokens"
+        data = {
+            "username": user,
+            "password": password,
+            "clientId": "iuSuHYVufIUuNIREV0FB9EoLn9kHsDbm",
+        }
+        headers = {"user-agent": "ACB-MBA/5 CFNetwork/1325.0.1 Darwin/21.1.0"}
+        req = requests.post(url, headers=headers, json=data)
+        print(req)
+        async with aiohttp.ClientSession(cookie_jar=aiohttp.CookieJar()) as session:
+            async with session.post(url, headers=headers, json=data) as res:
+                if res.status < 400:
+                    js = await res.json()
+                    headers["authorization"] = "Bearer " + js["refreshToken"]
+                    refreshTk = js["refreshToken"]
+                    url = "https://apiapp.acb.com.vn/mb/v2/auth/refresh"
+                    async with aiohttp.ClientSession(
+                        cookie_jar=aiohttp.CookieJar()
+                    ) as session:
+                        async with session.post(url, headers=headers) as res:
+                            url = "https://apiapp.acb.com.vn/mb/legacy/ss/cs/bankservice/transfers/list/account-payment"
+                            if res.status < 400:
+                                js = await res.json()
+                                headers["authorization"] = "Bearer " + js["accessToken"]
+                                print(user + " login success")
+                                return {
+                                    "headers": headers,
+                                    "refreshTk": refreshTk,
+                                    "username": user,
+                                }
+                return False
+    except Exception as err:
+        print(err)
+        return None
 
 
 async def getRefreshTk(headers):
