3f861fc91e7d177dc9587ea4c4b9ea746e7c03f3
alptangal <alptangal@gmail.com>
2026-02-09 11:22:02 +0700

    .

--- acb.py
+++ acb.py
@@ -24,56 +24,55 @@ async def login(user, password):
         headers = {"user-agent": "ACB-MBA/5 CFNetwork/1325.0.1 Darwin/21.1.0"}
         connector = aiohttp.TCPConnector(family=0)
         timeout = aiohttp.ClientTimeout(total=None, connect=None, sock_connect=None)
-        print(requests.Request("GET", "http://dummy").prepare().headers)
-        # res = requests.post(url, headers=headers, json=data)
-        # if res.status_code < 400:
-        #     js = res.json()
-        #     headers["authorization"] = "Bearer " + js["refreshToken"]
-        #     refreshTk = js["refreshToken"]
-        #     url = "https://apiapp.acb.com.vn/mb/v2/auth/refresh"
-        #     res = requests.post(url, headers=headers)
-        #     if res.status_code < 400:
-        #         js = res.json()
-        #         headers["authorization"] = "Bearer " + js["accessToken"]
-        #         print(user + " login success")
-        #         return {
-        #             "headers": headers,
-        #             "refreshTk": refreshTk,
-        #             "username": user,
-        #         }
-        # return False
-        async with aiohttp.ClientSession(
-            timeout=timeout, cookie_jar=aiohttp.CookieJar(), connector=connector
-        ) as session:
-            try:
-                async with session.post(
-                    url, headers=headers, json=data, ssl=False
-                ) as res:
-                    if res.status < 400:
-                        js = await res.json()
-                        headers["authorization"] = "Bearer " + js["refreshToken"]
-                        refreshTk = js["refreshToken"]
-                        url = "https://apiapp.acb.com.vn/mb/v2/auth/refresh"
-                        async with aiohttp.ClientSession(
-                            cookie_jar=aiohttp.CookieJar()
-                        ) as session:
-                            async with session.post(url, headers=headers) as res:
-                                url = "https://apiapp.acb.com.vn/mb/legacy/ss/cs/bankservice/transfers/list/account-payment"
-                                if res.status < 400:
-                                    js = await res.json()
-                                    headers["authorization"] = (
-                                        "Bearer " + js["accessToken"]
-                                    )
-                                    print(user + " login success")
-                                    return {
-                                        "headers": headers,
-                                        "refreshTk": refreshTk,
-                                        "username": user,
-                                    }
-                    return False
-            except Exception as err:
-                print(err)
-                return False
+        res = requests.post(url, headers=headers, json=data)
+        if res.status_code < 400:
+            js = res.json()
+            headers["authorization"] = "Bearer " + js["refreshToken"]
+            refreshTk = js["refreshToken"]
+            url = "https://apiapp.acb.com.vn/mb/v2/auth/refresh"
+            res = requests.post(url, headers=headers)
+            if res.status_code < 400:
+                js = res.json()
+                headers["authorization"] = "Bearer " + js["accessToken"]
+                print(user + " login success")
+                return {
+                    "headers": headers,
+                    "refreshTk": refreshTk,
+                    "username": user,
+                }
+        return False
+        # async with aiohttp.ClientSession(
+        #     timeout=timeout, cookie_jar=aiohttp.CookieJar(), connector=connector
+        # ) as session:
+        #     try:
+        #         async with session.post(
+        #             url, headers=headers, json=data, ssl=False
+        #         ) as res:
+        #             if res.status < 400:
+        #                 js = await res.json()
+        #                 headers["authorization"] = "Bearer " + js["refreshToken"]
+        #                 refreshTk = js["refreshToken"]
+        #                 url = "https://apiapp.acb.com.vn/mb/v2/auth/refresh"
+        #                 async with aiohttp.ClientSession(
+        #                     cookie_jar=aiohttp.CookieJar()
+        #                 ) as session:
+        #                     async with session.post(url, headers=headers) as res:
+        #                         url = "https://apiapp.acb.com.vn/mb/legacy/ss/cs/bankservice/transfers/list/account-payment"
+        #                         if res.status < 400:
+        #                             js = await res.json()
+        #                             headers["authorization"] = (
+        #                                 "Bearer " + js["accessToken"]
+        #                             )
+        #                             print(user + " login success")
+        #                             return {
+        #                                 "headers": headers,
+        #                                 "refreshTk": refreshTk,
+        #                                 "username": user,
+        #                             }
+        #             return False
+        #     except Exception as err:
+        #         print(err)
+        #         return False
     except Exception as err:
         print(err)
         return None
